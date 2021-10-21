a4bdb649c54a91de735eb18b8c2d11475ce32cd1
Dianne Plaza <d.plaza29@me.com>
2021-10-22 11:33:03 +1300

    create and update seed file

---
+++ server/db/seeds/dogs.js
@@ -0,0 +1,29 @@
+exports.seed = function (knex) {
+  return knex('dogs').del()
+    .then(function () {
+      return knex('dogs').insert([
+        {
+          id: 1,
+          user_id: 'auth0|61414f84d35ac900717bc280',
+          breed: 'Doberman',
+          name: 'Bam',
+          age: 2,
+          sex: 'male',
+          desexed: true,
+          bio: 'playful and loves the water',
+          image: './bam.png'
+        },
+        {
+          id: 2,
+          user_id: '20030bgf',
+          breed: 'Cavoodle',
+          name: 'Juno',
+          age: 4,
+          sex: 'female',
+          desexed: true,
+          bio: 'loves running and playing tag',
+          image: './juno.png'
+        }
+      ])
+    })
+}

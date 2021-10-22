e818bd19db2c1ec3dc06137a680c079cb4c18d79
kieran-tahir <kierantahir@gmail.com>
2021-10-22 16:49:19 +1300

    dogz

--- client/components/Dog.jsx
+++ client/components/Dog.jsx
@@ -7,16 +7,21 @@ function Dog (props) {
   console.log('dog.jsx is being hit: ', id)
   const dog = props.dogs.find(dog => dog.id == id)
   return (
-    <div>
-      <img className="dog-image" src={dog.image} alt={dog.name} />
-      <p>Name: {dog.name}</p>
-      <p>Breed: {dog.breed}</p>
-      <p>Age: {dog.age}</p>
-      <p>Sex: {dog.sex}</p>
-      <p>Desexed: {dog.desexed}</p>
-      <p>Bio: {dog.bio}</p>
-      <p>Location: {dog.location}</p>
-    </div>
+    <>
+      <div>
+        <button className='register-button'>Ask on a Date!</button>
+      </div>
+      <div>
+        <img className="dog-image" src={dog.image} alt={dog.name} />
+        <p>Name: {dog.name}</p>
+        <p>Breed: {dog.breed}</p>
+        <p>Age: {dog.age}</p>
+        <p>Sex: {dog.sex}</p>
+        <p>Desexed: {dog.desexed}</p>
+        <p>Bio: {dog.bio}</p>
+        <p>Location: {dog.location}</p>
+      </div>
+    </>
   )
 }
 

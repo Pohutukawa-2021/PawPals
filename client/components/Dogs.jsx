ca51a1a914ea2d8717cefdcfa08c507789aa800b
Zahira Champion <zahira.ali.champion@gmail.com>
2021-10-22 15:45:55 +1300

    create seperate boxes for each dog

--- client/components/Dogs.jsx
+++ client/components/Dogs.jsx
@@ -14,13 +14,15 @@ function Dogs (props) {
       {dogs.map(dog =>
         <div key={dog.id} className="dog-container">
           <img className="dog-image" src={dog.image} alt={dog.name} />
-          <p>Name: {dog.name}</p>
-          <p>Breed: {dog.breed}</p>
-          <p>Age: {dog.age}</p>
-          <p>Sex: {dog.sex}</p>
-          <p>Desexed: {dog.desexed}</p>
-          <p>Bio: {dog.bio}</p>
-          <p>Location: {dog.location}</p>
+          <div className="dog-info">
+            <p>Name: {dog.name}</p>
+            <p>Breed: {dog.breed}</p>
+            <p>Age: {dog.age}</p>
+            <p>Sex: {dog.sex}</p>
+            <p>Desexed: {dog.desexed}</p>
+            <p>Bio: {dog.bio}</p>
+            <p>Location: {dog.location}</p>
+          </div>
         </div>
       )}
     </div>
